e9ddd339119a01262a60c29666059a195087c240
Hidetake Iwata <int128@gmail.com>
2022-01-22 22:15:04 +0900

    Parse integer and show sum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let x = std::env::args().nth(1).expect("no name given");
-    println!("Hello, {}!", x);
+    let x: i32 = std::env::args().nth(1).expect("no x given").parse().expect("x must be integer");
+    let y: i32 = std::env::args().nth(2).expect("no y given").parse().expect("y must be integer");
+
+    println!("sum: {}", x + y);
 }
